acaea8c1f80e797f4401bbab7a62bc5d318932bd
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-04-16 18:40:08 +0100

    interface with static method

---
+++ src/main/java/interfaceStaticMethod/InterfaceStaticMethod.java
@@ -0,0 +1,10 @@
+package interfaceStaticMethod;
+
+/**
+ * Created by Ghazi Ennacer on 16/04/2017.
+ */
+public interface InterfaceStaticMethod {
+    static String display(){
+        return "MESSAGE";
+    }
+}
---
+++ src/main/java/interfaceStaticMethod/Sample.java
@@ -0,0 +1,10 @@
+package interfaceStaticMethod;
+
+/**
+ * Created by Ghazi Ennacer on 16/04/2017.
+ */
+public class Sample implements InterfaceStaticMethod {
+    public static void main(String[] args) {
+        System.out.println(InterfaceStaticMethod.display());
+    }
+}
