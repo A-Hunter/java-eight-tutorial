4fe865cb1778e6c7d14d51bb90562830db557a69
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-04-16 18:18:56 +0100

    Optional + filter

---
+++ src/main/java/optional/OptionalSample.java
@@ -0,0 +1,24 @@
+package optional;
+
+import consumer.person.Person;
+
+import java.util.Optional;
+
+/**
+ * Created by Ghazi Ennacer on 16/04/2017.
+ */
+public class OptionalSample {
+    public static void main(String[] args) {
+        Optional<Person> optionalOfperson = Optional.of(
+                new Person("Freecss", "Ging", 45, "Hunter"))
+                .filter(person -> {
+                    if (person.age <50) {
+                        String msg = person.lastName + " " + person.firstName + " : There is not too much information about him.";
+                        System.out.println(msg);
+                        return true;
+                    }
+                    return false;
+                });
+
+    }
+}
