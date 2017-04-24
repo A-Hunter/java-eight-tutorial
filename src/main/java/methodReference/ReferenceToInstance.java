7f9f469051f34f09b607247c1bf35946407e928b
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-04-24 23:59:50 +0100

    Reference to instance

--- src/main/java/methodReference/ReferenceToInstance.java
+++ src/main/java/methodReference/ReferenceToInstance.java
@@ -1,20 +1,26 @@
-//package methodReference;
-//
-///**
-// * Created by Ghazi Naceur on 24/04/2017.
-// */
-//public class ReferenceToInstance {
-//    public static void main(String args[]) {
-//        final List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-//        final MyComparator myComparator = new MyComparator();
-//        // Method reference
-//        Collections.sort(list, myComparator::compare);
-//        // Lambda expression
-//        Collections.sort(list, (a,b) -> myComparator.compare(a,b));
-//    }
-//    private static class MyComparator {
-//        public int compare(final Integer a, final Integer b) {
-//            return a.compareTo(b);
-//        }
-//    }
-//}
+package methodReference;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Ghazi Naceur on 24/04/2017.
+ */
+public class ReferenceToInstance {
+
+    public static void main(String args[]) {
+        final List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        final Comparator comparator = new Comparator();
+        // Method reference
+        Collections.sort(list, comparator::compare);
+        // Lambda expression
+        Collections.sort(list, (a,b) -> comparator.compare(a,b));
+    }
+    private static class Comparator {
+        private int compare(final Integer a, final Integer b) {
+            System.out.println(a.compareTo(b));
+            return a.compareTo(b);
+        }
+    }
+}
