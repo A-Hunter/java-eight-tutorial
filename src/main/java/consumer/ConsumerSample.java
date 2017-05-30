d1dd468ba31142a9d8268541b2fb7f6c3142ece6
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-05-30 01:57:03 +0100

    Suppress warnings

--- src/main/java/consumer/ConsumerSample.java
+++ src/main/java/consumer/ConsumerSample.java
@@ -3,25 +3,23 @@ package consumer;
 import consumer.person.Person;
 
 import java.util.function.Consumer;
-import java.util.logging.Logger;
 
 /**
  * Created by Ghazi Ennacer on 13/04/2017.
  */
 public class ConsumerSample {
 
-    public static Consumer<Person>  consume(){
-        Consumer<Person>  consumer = person -> {
-            StringBuilder sb = new StringBuilder(person.lastName)
-                    .append("-")
-                    .append(person.firstName)
-                    .append("-")
-                    .append(person.age)
-                    .append("-")
-                    .append(person.occupation);
-            System.out.println(sb.toString());
+    private static Consumer<Person>  consume(){
+        return person -> {
+            String sb = person.lastName +
+                    "-" +
+                    person.firstName +
+                    "-" +
+                    person.age +
+                    "-" +
+                    person.occupation;
+            System.out.println(sb);
         };
-       return consumer;
     }
 
     public static void main(String[] args) {
