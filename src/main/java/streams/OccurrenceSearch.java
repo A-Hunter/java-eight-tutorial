92222e8a79aaef989787d6adf7ca182ac7e0494b
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-06-15 08:38:21 +0100

    filter count

--- src/main/java/streams/OccurrenceSearch.java
+++ src/main/java/streams/OccurrenceSearch.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Created by Ghazi Naceur on 14/06/2017.
@@ -15,23 +16,25 @@ public class OccurrenceSearch {
         try {
             String searchWord = "test";
             String path = "E:\\GitHubRepositories\\java-eight-tutorial\\src\\main\\resources\\file.txt";
-            long count = 0;
-            ArrayList<String> words = new ArrayList<>();
-            BufferedReader reader = new BufferedReader(new FileReader(path));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                words.addAll(Arrays.asList(line.split(" ")));
-            }
-            for (String word : words){
-                if (word.equals(searchWord)) {
-                    count++;
-                }
-            }
-            System.out.println("The word " + searchWord + " occurred " + count + " time(s).");
-//            long count = Files.lines(Paths.get(path))
-//                 .filter(line -> line.contains(searchWord))
-//                 .count();
-//            System.out.println(count);
+//            long count = 0;
+//            ArrayList<String> words = new ArrayList<>();
+//            BufferedReader reader = new BufferedReader(new FileReader(path));
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//                words.addAll(Arrays.asList(line.split(" ")));
+//            }
+//            for (String word : words){
+//                if (word.equals(searchWord)) {
+//                    count++;
+//                }
+//            }
+//            System.out.println("The word " + searchWord + " occurred " + count + " time(s).");
+            //TODO : not completed
+            long count = Files.lines(Paths.get(path))
+                    .filter(line -> line.contains(searchWord))
+                    .count();
+            System.out.println(count);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
