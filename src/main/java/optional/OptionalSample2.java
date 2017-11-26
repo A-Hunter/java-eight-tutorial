ba022c1ca86dfeb78670dbdeabc1a6619e73a460
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-11-26 22:17:32 +0100

    optional example

---
+++ src/main/java/optional/OptionalSample2.java
@@ -0,0 +1,65 @@
+package optional;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class OptionalSample2 {
+
+    static class Outer {
+        Nested nested = new Nested();
+        Nested getNested(){
+            return nested;
+        }
+    }
+
+    static class Nested {
+        Inner inner = new Inner();
+        Inner getInner(){
+            return inner;
+        }
+    }
+
+    static class Inner {
+        String text = "text";
+        String getText(){
+            return text;
+        }
+    }
+
+    public static void main(String[] args){
+        test1();
+        test2();
+        test3();
+    }
+
+    private static Optional resolve(Supplier resolver){
+        try{
+            Object result = resolver.get();
+            return Optional.ofNullable(result);
+        }catch (NullPointerException e){
+            return Optional.empty();
+        }
+    }
+
+    private static void test3(){
+        Outer outer = new Outer();
+        resolve(() -> outer.getNested().getInner().getText())
+                .ifPresent(System.out::println);
+    }
+
+    private static void test2(){
+        Optional.of(new Outer())
+                .map(Outer::getNested)
+                .map(Nested::getInner)
+                .map(Inner::getText)
+                .ifPresent(System.out::println);
+    }
+
+    private static void test1(){
+        Optional.of(new Outer())
+                .flatMap(o -> Optional.ofNullable(o.nested))
+                .flatMap(n -> Optional.ofNullable(n.inner))
+                .flatMap(i -> Optional.ofNullable(i.text))
+                .ifPresent(System.out::println);
+    }
+}
